de675454ab61ecbf9ba71adf1ced4b7e5a6d4e1e
Guilherme Oliveira <guilherme.oliveira@gatec.com.br>
2023-01-09 14:27:12 -0300

    Componentes

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import Logo from './componentes/Logo'
 import perfil from './imagens/perfil.svg'
 import sacola from './imagens/sacola.svg'
+import OpcoesHeader from './componentes/OpcoesHeader'
 
-const textoOpcoes = ['CATEGORIAS','FAVORITOS', 'MINHA ESTANTE']
 const icones = [perfil, sacola]
 
 function App() {
@@ -11,11 +11,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <Logo></Logo>
-        <ul className='opcoes'>
-          {textoOpcoes.map((texto) => (
-            <li className='opcao'><p>{texto}</p></li>
-          ))}
-        </ul>
+        <OpcoesHeader></OpcoesHeader>        
         <ul className='icones'>
           {icones.map((icone) =>(
             <li className='icone'><img src = {icone}></img></li>
